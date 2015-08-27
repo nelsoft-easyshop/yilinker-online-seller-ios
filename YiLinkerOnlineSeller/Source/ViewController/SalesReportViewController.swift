99c62c011b3b9f44dab710f63a3c4433121ad0f4
jpChan15 <john.chan@easyshop.ph>
2015-08-27 11:44:42 +0800

    Implemented sales report. (issue #10)

--- YiLinkerOnlineSeller/Source/ViewController/SalesReportViewController.swift
+++ YiLinkerOnlineSeller/Source/ViewController/SalesReportViewController.swift
@@ -8,34 +8,53 @@
 
 import UIKit
 
-class SalesReportViewController: UIViewController {
+class SalesReportViewController: UIViewController, UISearchBarDelegate {
+    
+    var recentProductsController: RecentProductsTableViewController?
+    var salesReportController: SalesReportTableViewController?
+    
+    @IBOutlet weak var searchHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var salesReportsView: UIView!
     @IBOutlet weak var salesReportImageView: UIImageView!
     @IBOutlet weak var salesReportLabel: UILabel!
-    
     @IBOutlet weak var recentOrdersView: UIView!
     @IBOutlet weak var recentOrdersImageView: UIImageView!
     @IBOutlet weak var recentOrdersLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
     
-    var isSalesReport: Bool = true
-
+    var searchButton: UIButton!
+    
+    var currentView: String = "salesReportsView"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        initializeViewControllers()
         initializeViews()
         initializeNavigationBar()
         initializeTapGesture()
+        
+        changeViewController(salesReportController!)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func initializeViewControllers() {
+        recentProductsController = RecentProductsTableViewController(nibName: "RecentProductsTableViewController", bundle: nil)
+        
+        salesReportController = SalesReportTableViewController(nibName: "SalesReportTableViewController", bundle: nil)
     }
     
     func initializeViews() {
         if self.respondsToSelector("edgesForExtendedLayout") {
             self.edgesForExtendedLayout = UIRectEdge.None
         }
+        
+        self.searchBar.delegate = self
     }
     
     func initializeNavigationBar() {
@@ -50,10 +69,26 @@ class SalesReportViewController: UIViewController {
         let navigationSpacer: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FixedSpace, target: nil, action: nil)
         navigationSpacer.width = -20
         self.navigationItem.leftBarButtonItems = [navigationSpacer, customBackButton]
+        
+        searchButton = UIButton.buttonWithType(UIButtonType.Custom) as? UIButton
+        searchButton.frame = CGRectMake(0, 0, 25, 25)
+        searchButton.addTarget(self, action: "search", forControlEvents: UIControlEvents.TouchUpInside)
+        searchButton.setImage(UIImage(named: "search-white"), forState: UIControlState.Normal)
+        searchButton.hidden = true
+        var customCheckButton:UIBarButtonItem = UIBarButtonItem(customView: searchButton)
+        
+        let navigationSpacer2: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FixedSpace, target: nil, action: nil)
+        navigationSpacer2.width = -10
+        
+        self.navigationItem.rightBarButtonItems = [navigationSpacer2, customCheckButton]
     }
     
     func back() {
         self.navigationController!.popViewControllerAnimated(true)
+    }
+    
+    func search() {
+        setSearchBarHidden(false)
     }
     
     func initializeTapGesture() {
@@ -70,6 +105,9 @@ class SalesReportViewController: UIViewController {
         let senderView: UIView = sender.view!
         
         if senderView == salesReportsView {
+            setSearchBarHidden(true)
+            searchButton.hidden = true
+            
             salesReportsView.backgroundColor = UIColor.whiteColor()
             salesReportLabel.textColor = Constants.Colors.productPrice
             salesReportImageView.image = UIImage(named: "salesReport")
@@ -77,7 +115,14 @@ class SalesReportViewController: UIViewController {
             recentOrdersView.backgroundColor = Constants.Colors.productPrice
             recentOrdersLabel.textColor = UIColor.whiteColor()
             recentOrdersImageView.image = UIImage(named: "recentOrders2")
+            
+            if currentView != "salesReportsView" {
+                changeViewController(salesReportController!)
+                currentView = "salesReportsView"
+            }
         } else {
+            searchButton.hidden = false
+            
             salesReportsView.backgroundColor = Constants.Colors.productPrice
             salesReportLabel.textColor = UIColor.whiteColor()
             salesReportImageView.image = UIImage(named: "salesReport2")
@@ -85,17 +130,70 @@ class SalesReportViewController: UIViewController {
             recentOrdersView.backgroundColor = UIColor.whiteColor()
             recentOrdersLabel.textColor = Constants.Colors.productPrice
             recentOrdersImageView.image = UIImage(named: "recentOrders")
+            
+            if currentView != "recentOrdersView" {
+                changeViewController(recentProductsController!)
+                currentView = "recentOrdersView"
+            }
+        }
+        
+    }
+    
+    //Change of View Controller
+    func changeViewController(viewController: UIViewController) {
+        self.addChildViewController(viewController)
+        viewController.view.frame = self.containerView.frame
+        viewController.view.frame.origin.y = 0
+        containerView.addSubview(viewController.view)
+        viewController.didMoveToParentViewController(self)
+    }
+    
+    // Mark: - UISearchBarDelegate
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        self.searchBar.sizeToFit()
+        self.searchBar.setShowsCancelButton(true, animated: true)
+        return true
+    }
+    
+    func searchBarShouldEndEditing(searchBar: UISearchBar) -> Bool {
+        self.searchBar.sizeToFit()
+        self.searchBar.setShowsCancelButton(false, animated: true)
+        return true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        self.searchBar.resignFirstResponder()
+        setSearchBarHidden(true)
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+    }
+    
+    // Hide Show Search bar
+    func setSearchBarHidden(hidden: Bool) {
+        if hidden {
+            UIView.animateWithDuration(0.5, animations: {
+                self.searchHeightConstraint.constant = 0
+                self.view.layoutIfNeeded()
+            })
+            self.searchBar.resignFirstResponder()
+            self.searchBar.text = ""
+        } else {
+            UIView.animateWithDuration(0.5, animations: {
+                self.searchHeightConstraint.constant = 44
+                self.view.layoutIfNeeded()
+            })
         }
     }
     
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
