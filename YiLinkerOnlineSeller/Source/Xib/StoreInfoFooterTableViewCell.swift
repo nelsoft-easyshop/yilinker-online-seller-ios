766b40acad7449be65a65bd19c69ae8de5a47e31
Yhel <joriel.fronda@easyshop.ph>
2015-08-30 10:09:56 -0700

    Added layout for store infor table view cell.

---
+++ YiLinkerOnlineSeller/Source/Xib/StoreInfoFooterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StoreInfoFooterTableViewCell.swift
+//  
+//
+//  Created by Joriel Oller Fronda on 8/30/15.
+//
+//
+
+import UIKit
+
+class StoreInfoFooterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ YiLinkerOnlineSeller/Source/Xib/StoreInfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StoreInfoViewController.swift
+//  YiLinkerOnlineSeller
+//
+//  Created by Joriel Oller Fronda on 8/29/15.
+//  Copyright (c) 2015 YiLinker. All rights reserved.
+//
+
+import UIKit
+
+class StoreInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
