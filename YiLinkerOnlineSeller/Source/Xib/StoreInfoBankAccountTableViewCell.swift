7f0e003a915ae13423e8403c08d69358ce3d9d50
Yhel <joriel.fronda@easyshop.ph>
2015-08-30 10:24:42 -0700

    Added new files for store info

---
+++ YiLinkerOnlineSeller/Source/Xib/StoreInfoBankAccountTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StoreInfoBankAccountTableViewCell.swift
+//  YiLinkerOnlineSeller
+//
+//  Created by Joriel Oller Fronda on 8/30/15.
+//  Copyright (c) 2015 YiLinker. All rights reserved.
+//
+
+import UIKit
+
+class StoreInfoBankAccountTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
