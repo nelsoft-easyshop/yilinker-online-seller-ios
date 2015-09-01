86cbfadd5f92e4538e647d4d8c5819c4c337d3d7
Yhel <joriel.fronda@easyshop.ph>
2015-09-01 16:47:04 +0800

    Added model for store info

---
+++ YiLinkerOnlineSeller/Source/Model/StoreInfoModel.swift
@@ -0,0 +1,9 @@
+//
+//  StoreInfoModel.swift
+//  YiLinkerOnlineSeller
+//
+//  Created by Joriel Oller Fronda on 9/1/15.
+//  Copyright (c) 2015 YiLinker. All rights reserved.
+//
+
+import Foundation
