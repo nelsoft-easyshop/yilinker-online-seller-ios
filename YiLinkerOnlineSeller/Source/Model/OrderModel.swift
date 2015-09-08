b8c86a95ab8c700d1469b3e023a3f1cb4572a2ee
jpChan15 <john.chan@easyshop.ph>
2015-09-08 11:52:27 +0800

    Implemented sales report api.

---
+++ YiLinkerOnlineSeller/Source/Model/OrderModel.swift
@@ -0,0 +1,13 @@
+//
+//  OrderModel.swift
+//  YiLinkerOnlineSeller
+//
+//  Created by John Paul Chan on 9/8/15.
+//  Copyright (c) 2015 YiLinker. All rights reserved.
+//
+
+import UIKit
+
+class OrderModel: NSObject {
+   
+}
