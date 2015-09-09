e42d538a739d1ad2a781c1cafd7efa7a55d30851
Yhel <joriel.fronda@easyshop.ph>
2015-09-09 12:08:54 +0800

    Added model for search.

---
+++ YiLinkerOnlineSeller/Source/Model/SearchModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchModel.swift
+//  YiLinkerOnlineSeller
+//
+//  Created by Joriel Oller Fronda on 9/8/15.
+//  Copyright (c) 2015 YiLinker. All rights reserved.
+//
+
+import UIKit
+
+class SearchModel: NSObject {
+   
+}
