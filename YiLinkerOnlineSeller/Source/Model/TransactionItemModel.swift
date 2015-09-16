4a40a684d5de98b572ec18de84b378623a3f0c0c
jpChan15 <john.chan@easyshop.ph>
2015-09-16 19:44:14 +0800

    Added model for Transaction Items.

---
+++ YiLinkerOnlineSeller/Source/Model/TransactionItemModel.swift
@@ -0,0 +1,13 @@
+//
+//  TransactionItemModel.swift
+//  YiLinkerOnlineSeller
+//
+//  Created by John Paul Chan on 9/16/15.
+//  Copyright (c) 2015 YiLinker. All rights reserved.
+//
+
+import UIKit
+
+class TransactionItemModel: NSObject {
+   
+}
