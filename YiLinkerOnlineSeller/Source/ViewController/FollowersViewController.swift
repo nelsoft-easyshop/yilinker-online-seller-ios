45f83fd507a0b50d3d5a21f9e7da5a6d536461d9
jpChan15 <john.chan@easyshop.ph>
2015-09-23 20:36:32 +0800

    Implemented language localization in followers.

--- YiLinkerOnlineSeller/Source/ViewController/FollowersViewController.swift
+++ YiLinkerOnlineSeller/Source/ViewController/FollowersViewController.swift
@@ -20,10 +20,16 @@ class FollowersViewController: UIViewController, UISearchBarDelegate, UITableVie
     
     var getCtr: Int = 0
     
+    var errorLocalizeString: String  = ""
+    var somethingWrongLocalizeString: String = ""
+    var connectionLocalizeString: String = ""
+    var connectionMessageLocalizeString: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         initializeViews()
+        initializeLocalizedString()
         titleView()
         backButton()
         registerNibs()
@@ -40,6 +46,14 @@ class FollowersViewController: UIViewController, UISearchBarDelegate, UITableVie
         // Dispose of any resources that can be recreated.
     }
     
+    func initializeLocalizedString() {
+        //Initialized Localized String
+        errorLocalizeString = StringHelper.localizedStringWithKey("ERROR_LOCALIZE_KEY")
+        somethingWrongLocalizeString = StringHelper.localizedStringWithKey("SOMETHINGWENTWRONG_LOCALIZE_KEY")
+        connectionLocalizeString = StringHelper.localizedStringWithKey("CONNECTIONUNREACHABLE_LOCALIZE_KEY")
+        connectionMessageLocalizeString = StringHelper.localizedStringWithKey("CONNECTIONERRORMESSAGE_LOCALIZE_KEY")
+    }
+    
     
     func initializeViews() {
         if self.respondsToSelector("edgesForExtendedLayout") {
@@ -54,10 +68,12 @@ class FollowersViewController: UIViewController, UISearchBarDelegate, UITableVie
         self.tableView.tableFooterView = UIView(frame: CGRectZero)
         
         emptyLabel.hidden = true
+        emptyLabel.text = StringHelper.localizedStringWithKey("NO_FOLLOWER_LOCALIZE_KEY")
         
         self.tableView.delegate = self
         self.tableView.dataSource = self
         searchBar.delegate = self
+        
     }
     
     func registerNibs() {
@@ -66,7 +82,7 @@ class FollowersViewController: UIViewController, UISearchBarDelegate, UITableVie
     }
     
     func titleView() {
-        self.title = "Followers"
+        self.title = StringHelper.localizedStringWithKey("FOLLOWERS_TITLE_LOCALIZE_KEY")
     }
     
     func backButton() {
@@ -197,7 +213,7 @@ class FollowersViewController: UIViewController, UISearchBarDelegate, UITableVie
                 
                 
             } else {
-                UIAlertController.displayErrorMessageWithTarget(self, errorMessage: self.followersModel.message, title: "Error")
+                UIAlertController.displayErrorMessageWithTarget(self, errorMessage: self.followersModel.message, title: self.errorLocalizeString)
             }
             self.hud?.hide(true)
             
@@ -212,9 +228,9 @@ class FollowersViewController: UIViewController, UISearchBarDelegate, UITableVie
                     } else {
                         self.emptyLabel.hidden = false
                         if Reachability.isConnectedToNetwork() {
-                            UIAlertController.displayErrorMessageWithTarget(self, errorMessage: "Something went wrong!", title: "Error")
+                            UIAlertController.displayErrorMessageWithTarget(self, errorMessage: self.somethingWrongLocalizeString, title: self.errorLocalizeString)
                         } else {
-                            UIAlertController.displayErrorMessageWithTarget(self, errorMessage: "Check your internet connection!", title: "Error")
+                            UIAlertController.displayErrorMessageWithTarget(self, errorMessage: self.connectionMessageLocalizeString, title: self.connectionLocalizeString)
                         }
                         println(error)
                     }
