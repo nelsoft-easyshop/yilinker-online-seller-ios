fb369f8fc0c3de00bcabb611ddb69b0df5de8f96
Yhel <joriel.fronda@easyshop.ph>
2015-10-04 14:07:19 +0800

    Implemented refresh tokens.

--- YiLinkerOnlineSeller/Source/ViewController/VerifyNumberViewController.swift
+++ YiLinkerOnlineSeller/Source/ViewController/VerifyNumberViewController.swift
@@ -58,7 +58,7 @@ class VerifyNumberViewController: UIViewController {
     let ok: String = StringHelper.localizedStringWithKey("CHANGE_MOBILE_OK_LOCALIZE_KEY")
     let somethingWentWrong: String = StringHelper.localizedStringWithKey("ERROR_SOMETHING_WENT_WRONG_LOCALIZE_KEY")
     let expired: String = StringHelper.localizedStringWithKey("VERIFY_NUMBER_EXPIRED_LOCALIZE_KEY")
-    
+    let information: String = StringHelper.localizedStringWithKey("STORE_INFO_INFORMATION_TITLE_LOCALIZE_KEY")
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -117,17 +117,30 @@ class VerifyNumberViewController: UIViewController {
         manager.POST(APIAtlas.sellerResendVerification+"\(SessionManager.accessToken())&mobileNumber=\(self.mobileNumber)", parameters: nil, success: {
             (task: NSURLSessionDataTask!, responseObject: AnyObject!) in
                 if responseObject["isSuccessful"] as! Bool {
+                    self.showAlert(self.information, message: responseObject["message"] as! String )
                     self.seconds = 300
                     self.invalidateTimer()
                 } else {
-                    self.showAlert(self.error, message: self.somethingWentWrong)
+                    self.showAlert(self.error, message: responseObject["message"] as! String)
                 }
                 println(responseObject.description)
                 //self.setSelectedViewControllerWithIndex(0)
                 self.hud?.hide(true)
             }, failure: { (task: NSURLSessionDataTask!, error: NSError!) in
                 self.hud?.hide(true)
-                self.showAlert(self.error, message: self.somethingWentWrong)
+                let task: NSHTTPURLResponse = task.response as! NSHTTPURLResponse
+                if error.userInfo != nil {
+                    let dictionary: NSDictionary = (error.userInfo as? Dictionary<String, AnyObject>)!
+                    let errorModel: ErrorModel = ErrorModel.parseErrorWithResponce(dictionary)
+                    self.showAlert(self.error, message: errorModel.message)
+                    self.delegate?.congratulationsViewController(false)
+                } else if task.statusCode == 401 {
+                    self.requestRefreshToken(VerifyType.Resend)
+                } else {
+                    self.delegate?.congratulationsViewController(false)
+                    self.showAlert(self.error, message: self.somethingWentWrong)
+                }
+                //self.showAlert(self.error, message: self.somethingWentWrong)
         })
     }
     
@@ -140,28 +153,63 @@ class VerifyNumberViewController: UIViewController {
             
             manager.POST(APIAtlas.sellerMobileNumberVerification, parameters: parameters, success: {
                 (task: NSURLSessionDataTask!, responseObject: AnyObject!) in
+                self.hud?.hide(true)
                 if responseObject["isSuccessful"] as! Bool {
                     self.dismissViewControllerAnimated(true, completion: nil)
-                    self.hud?.hide(true)
                     //self.delegate?.dismissView()
                     self.delegate?.congratulationsViewController(true)
                 } else {
                     println("\(responseObject)")
-                    self.showAlert(self.error, message: self.somethingWentWrong)
+                    self.showAlert(self.error, message: responseObject["message"] as! String)
                 }
             }, failure: { (task: NSURLSessionDataTask!, error: NSError!) in
                 self.dismissViewControllerAnimated(true, completion: nil)
                 self.hud?.hide(true)
                 //self.delegate?.dismissView()
-                println(error.userInfo)
-                self.delegate?.congratulationsViewController(false)
-                self.showAlert(self.error, message: self.somethingWentWrong)
-                println(error)
+                let task: NSHTTPURLResponse = task.response as! NSHTTPURLResponse
+                if error.userInfo != nil {
+                    let dictionary: NSDictionary = (error.userInfo as? Dictionary<String, AnyObject>)!
+                    let errorModel: ErrorModel = ErrorModel.parseErrorWithResponce(dictionary)
+                    self.showAlert(self.error, message: errorModel.message)
+                    self.delegate?.congratulationsViewController(false)
+                } else if task.statusCode == 401 {
+                    self.requestRefreshToken(VerifyType.Verify)
+                } else {
+                    self.delegate?.congratulationsViewController(false)
+                    self.showAlert(self.error, message: self.somethingWentWrong)
+                }
+                //println(error.userInfo)
+                //self.delegate?.congratulationsViewController(false)
+                //self.showAlert(self.error, message: self.somethingWentWrong)
+                //println(error)
             })
         } else {
              self.showAlert(self.error, message: self.expired)
         }
        
+    }
+    
+    func requestRefreshToken(type: VerifyType) {
+        let params: NSDictionary = ["client_id": Constants.Credentials.clientID,
+            "client_secret": Constants.Credentials.clientSecret,
+            "grant_type": Constants.Credentials.grantRefreshToken,
+            "refresh_token": SessionManager.refreshToken()]
+        self.showHUD()
+        let manager = APIManager.sharedInstance
+        manager.POST(APIAtlas.loginUrl, parameters: params, success: {
+            (task: NSURLSessionDataTask!, responseObject: AnyObject!) in
+            
+            if type == VerifyType.Verify {
+                self.fireVerify(self.verificationCodeTextField.text)
+            } else if type == VerifyType.Resend {
+                self.fireResendVerificationCode()
+            }
+            
+            }, failure: {
+                (task: NSURLSessionDataTask!, error: NSError!) in
+                self.hud?.hide(true)
+                 self.showAlert(self.error, message: self.somethingWentWrong)
+        })
     }
     
     //MARK: Show MBProgressHUD bar
