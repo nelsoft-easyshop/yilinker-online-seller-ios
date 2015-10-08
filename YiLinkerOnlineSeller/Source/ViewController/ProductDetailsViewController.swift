fd855c2f883b2ac40d50fee23b31f92623047270
Rj <rj.constantino@easyshop.ph>
2015-10-08 23:49:29 +0800

    Enhanced edit action when loading.

--- YiLinkerOnlineSeller/Source/ViewController/ProductDetailsViewController.swift
+++ YiLinkerOnlineSeller/Source/ViewController/ProductDetailsViewController.swift
@@ -301,38 +301,40 @@ class ProductDetailsViewController: UIViewController, UITableViewDataSource, UIT
     }
     
     func editAction() {
-        self.showHUD()
-        if self.productModel.imageUrls.count != self.productModel.images.count {
-            self.productModel.images = []
-            for i in 0..<self.productModel.imageUrls.count {
-                var imgURL: NSURL = NSURL(string: self.productModel.imageUrls[i])!
-                let request: NSURLRequest = NSURLRequest(URL: imgURL)
-                NSURLConnection.sendAsynchronousRequest(
-                    request, queue: NSOperationQueue.mainQueue(),
-                    completionHandler: {(response: NSURLResponse!,data: NSData!,error: NSError!) -> Void in
-                        if error == nil {
-                            var convertedImage: ServerUIImage = ServerUIImage(data: data)!
-                            convertedImage.uid = self.productModel.imageIds[i]
-                            self.productModel.images.append(convertedImage)
-                            if self.productModel.images.count == self.productModel.imageUrls.count {
-                                self.hud?.hide(true)
-                                let upload = ProductUploadTableViewController(nibName: "ProductUploadTableViewController", bundle: nil)
-                                upload.uploadType = UploadType.EditProduct
-                                upload.productModel = self.productModel
-                                let navigationController: UINavigationController = UINavigationController(rootViewController: upload)
-                                navigationController.navigationBar.barTintColor = Constants.Colors.appTheme
-                                self.tabBarController!.presentViewController(navigationController, animated: true, completion: nil)
+        if self.productModel != nil {
+            self.showHUD()
+            if self.productModel.imageUrls.count != self.productModel.images.count {
+                self.productModel.images = []
+                for i in 0..<self.productModel.imageUrls.count {
+                    var imgURL: NSURL = NSURL(string: self.productModel.imageUrls[i])!
+                    let request: NSURLRequest = NSURLRequest(URL: imgURL)
+                    NSURLConnection.sendAsynchronousRequest(
+                        request, queue: NSOperationQueue.mainQueue(),
+                        completionHandler: {(response: NSURLResponse!,data: NSData!,error: NSError!) -> Void in
+                            if error == nil {
+                                var convertedImage: ServerUIImage = ServerUIImage(data: data)!
+                                convertedImage.uid = self.productModel.imageIds[i]
+                                self.productModel.images.append(convertedImage)
+                                if self.productModel.images.count == self.productModel.imageUrls.count {
+                                    self.hud?.hide(true)
+                                    let upload = ProductUploadTableViewController(nibName: "ProductUploadTableViewController", bundle: nil)
+                                    upload.uploadType = UploadType.EditProduct
+                                    upload.productModel = self.productModel
+                                    let navigationController: UINavigationController = UINavigationController(rootViewController: upload)
+                                    navigationController.navigationBar.barTintColor = Constants.Colors.appTheme
+                                    self.tabBarController!.presentViewController(navigationController, animated: true, completion: nil)
+                                }
                             }
-                        }
-                })
+                    })
+                }
+            } else {
+                let upload = ProductUploadTableViewController(nibName: "ProductUploadTableViewController", bundle: nil)
+                upload.uploadType = UploadType.EditProduct
+                upload.productModel = self.productModel
+                let navigationController: UINavigationController = UINavigationController(rootViewController: upload)
+                navigationController.navigationBar.barTintColor = Constants.Colors.appTheme
+                self.tabBarController!.presentViewController(navigationController, animated: true, completion: nil)
             }
-        } else {
-            let upload = ProductUploadTableViewController(nibName: "ProductUploadTableViewController", bundle: nil)
-            upload.uploadType = UploadType.EditProduct
-            upload.productModel = self.productModel
-            let navigationController: UINavigationController = UINavigationController(rootViewController: upload)
-            navigationController.navigationBar.barTintColor = Constants.Colors.appTheme
-            self.tabBarController!.presentViewController(navigationController, animated: true, completion: nil)
         }
     }
 
