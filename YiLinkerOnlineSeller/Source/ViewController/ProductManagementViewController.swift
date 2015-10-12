1e1ce2fb25845cca353faebc6873438cdb07ee48
Yhel <joriel.fronda@easyshop.ph>
2015-10-12 23:52:01 +0800

    Resolved new status values.

--- YiLinkerOnlineSeller/Source/ViewController/ProductManagementViewController.swift
+++ YiLinkerOnlineSeller/Source/ViewController/ProductManagementViewController.swift
@@ -34,9 +34,9 @@ private struct ManagementStrings {
 
 private struct Status {
     static let active = 2
-    static let inactive = 3
+    static let inactive = 6
     static let draft = 0
-    static let deleted = 4
+    static let deleted = 3
     static let review = 1
     static let rejected = 5
 }
@@ -67,7 +67,8 @@ class ProductManagementViewController: UIViewController, ProductManagementModelV
     var selectedImage: [String] = ["all2", "active2", "inactive2", "drafts2", "deleted2", "review2", "review2"]
     var deSelectedImage: [String] = ["all", "active", "inactive", "drafts", "deleted", "review", "review"]
 //status : Active = 2, Inactive = 3, Draft = 0, Deleted =4, For Review = 1, Rejected = 5, All = all
-    var statusId: [Int] = [6, 2, 3, 0, 4, 1, 5]
+//    draft = 0, review = 1, active = 2, delete = 3, reject = 5, inactive = 6
+    var statusId: [Int] = [7, 2, 6, 0, 3, 1, 5]
     
     var selectedIndex: Int = 0
     var tableViewSectionHeight: CGFloat = 0
@@ -86,7 +87,7 @@ class ProductManagementViewController: UIViewController, ProductManagementModelV
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        requestGetProductList(6, key: "")
+        requestGetProductList(7, key: "")
         customizeNavigationBar()
         customizeViews()
         registerNibs()
@@ -189,7 +190,7 @@ class ProductManagementViewController: UIViewController, ProductManagementModelV
         
         if selectedIndex == 1 {
             button1.setTitle(ManagementStrings.disableAll, forState: .Normal)
-        } else if selectedIndex == 2 || selectedIndex == 3 {
+        } else if selectedIndex == 2 || selectedIndex == 6 {
             button1.setTitle(ManagementStrings.deleteAll, forState: .Normal)
             
             if selectedIndex == 2 {
@@ -316,7 +317,7 @@ class ProductManagementViewController: UIViewController, ProductManagementModelV
                 } else if sender.titleLabel!!.text == ManagementStrings.restoreAll {
                     action = Status.active
                 }
-            } else if selectedIndex == 3 {
+            } else if selectedIndex == 6 {
                 action = Status.deleted
             }
             
@@ -368,7 +369,7 @@ class ProductManagementViewController: UIViewController, ProductManagementModelV
             self.showHUD()
             
             var parameters: NSDictionary = [:]
-            if status == 6 {
+            if status == 7 {
                 parameters = ["access_token": SessionManager.accessToken(),
                     "status": "all",
                     "keyword": key]
@@ -475,7 +476,7 @@ extension ProductManagementViewController: UITextFieldDelegate, UITableViewDataS
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        if selectedIndex == 0 || selectedIndex == 6 {
+        if selectedIndex == 0 || selectedIndex == 7 {
             let cell: ProductManagementAllTableViewCell = self.tableView.dequeueReusableCellWithIdentifier("ProductManagementAllIdentifier") as! ProductManagementAllTableViewCell
             cell.selectionStyle = .None
             
@@ -483,7 +484,7 @@ extension ProductManagementViewController: UITextFieldDelegate, UITableViewDataS
             cell.titleLabel.text = self.productModel.products[indexPath.row].name
             cell.subTitleLabel.text = self.productModel.products[indexPath.row].category
             cell.setStatus(self.productModel.products[indexPath.row].status)
-            if selectedIndex == 6 {
+            if selectedIndex == 7 {
                 cell.statusLabel.hidden = true
             } else {
                 cell.statusLabel.hidden = false
@@ -509,7 +510,7 @@ extension ProductManagementViewController: UITextFieldDelegate, UITableViewDataS
             cell.titleLabel.text = self.productModel.products[indexPath.row].name
             cell.subTitleLabel.text = self.productModel.products[indexPath.row].category
 
-            if selectedIndex == 4 {
+            if selectedIndex == 3 {
                 cell.arrowImageView.hidden = true
                 cell.decreaseAlpha()
             } else {
@@ -538,13 +539,13 @@ extension ProductManagementViewController: UITextFieldDelegate, UITableViewDataS
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         if Reachability.isConnectedToNetwork() {
-            if selectedIndex != 4 {
+            if selectedIndex != 3 {
                 let productDetails = ProductDetailsViewController(nibName: "ProductDetailsViewController", bundle: nil)
                 productDetails.productId = self.productModel.products[indexPath.row].id
-                if selectedIndex != 6 {
+                if selectedIndex != 7 {
                     productDetails.isEditable = true
                     if selectedIndex == 0 {
-                        if self.productModel.products[indexPath.row].status == 4 || self.productModel.products[indexPath.row].status == 1 {
+                        if self.productModel.products[indexPath.row].status == 3 || self.productModel.products[indexPath.row].status == 1 {
                             productDetails.isEditable = false
                         }
                     }
@@ -646,12 +647,12 @@ extension ProductManagementViewController: UITextFieldDelegate, UITableViewDataS
             self.deleteView.hidden = true
             self.activeInactiveContainerView.hidden = true
             
-        } else if selectedIndex == 3 || selectedIndex == 6 {
+        } else if selectedIndex == 6 || selectedIndex == 7 {
             self.deleteView.hidden = false
             
             self.activeInactiveDeleteContainerView.hidden = true
             self.activeInactiveContainerView.hidden = true
-        } else if selectedIndex == 4 {
+        } else if selectedIndex == 3 {
             self.activeInactiveContainerView.hidden = false
             
             self.deleteView.hidden = true
