dab710f0731d529c03f5054873c2d4825f4a252f
Yhel <joriel.fronda@easyshop.ph>
2016-01-23 16:25:20 +0800

    Refactored code - Add product header view.

--- YiLinkerOnlineSeller/Source/View/AddProductHeaderView.swift
+++ YiLinkerOnlineSeller/Source/View/AddProductHeaderView.swift
@@ -8,20 +8,28 @@
 
 import UIKit
 
+// MARK: - Delegate
+// AddProductHeaderView Delegate method
 protocol AddProductHeaderViewDelegate {
     func addProductHeaderView(addProductHeaderView: AddProductHeaderView, didClickButtonAdd button: UIButton)
 }
 
 class AddProductHeaderView: UIView {
-
-    var delegate: AddProductHeaderViewDelegate?
+    
+    // Buttons
     @IBOutlet weak var addButton: UIButton!
+    
+    // Labels
     @IBOutlet weak var productsLabel: UILabel!
    
+    // Initialized AddProductHeaderViewDelegate
+    var delegate: AddProductHeaderViewDelegate?
+    
     override func awakeFromNib() {
         self.addButton.layer.cornerRadius = 5
     }
 
+    // MARK: - Button action
     @IBAction func addItem(sender: AnyObject) {
         self.delegate?.addProductHeaderView(self, didClickButtonAdd: self.addButton)
     }
