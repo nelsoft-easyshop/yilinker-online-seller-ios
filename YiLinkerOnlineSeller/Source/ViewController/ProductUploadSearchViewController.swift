61a8164dc8cf2b910aab70705b0833f63084ec86
Yhel <joriel.fronda@easyshop.ph>
2016-01-25 18:18:53 +0800

    Refactor ProductUploadSearchViewController.

--- YiLinkerOnlineSeller/Source/ViewController/ProductUploadSearchViewController.swift
+++ YiLinkerOnlineSeller/Source/ViewController/ProductUploadSearchViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 
 class ProductUploadSearchViewController: UIViewController, UISearchBarDelegate {
 
+    // Table view
     @IBOutlet weak var tableView: UITableView!
+    
+    // Global variable
     var searchBar: UISearchBar?
     
     override func viewDidLoad() {
@@ -23,6 +26,7 @@ class ProductUploadSearchViewController: UIViewController, UISearchBarDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: Initialize search bar
     func search() {
         self.view.layoutIfNeeded()
         self.searchBar = UISearchBar(frame: CGRectMake(0, 0, self.view.frame.size.width - 30, 44))
@@ -34,6 +38,7 @@ class ProductUploadSearchViewController: UIViewController, UISearchBarDelegate {
         self.navigationItem.rightBarButtonItem =  UIBarButtonItem(customView: searchBar!)
     }
     
+    // MARK: Serach bar delegate methods
     func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
         
     }
