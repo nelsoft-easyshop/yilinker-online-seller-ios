c041a98eaf263e292bca6e2ba7917246a1bd61dd
Yhel <joriel.fronda@easyshop.ph>
2016-01-29 16:47:39 +0800

    Added codel for payout request list.

---
+++ YiLinkerOnlineSeller/Source/Model/PayoutRequestListModel.swift
@@ -0,0 +1,13 @@
+//
+//  PayoutRequestListModel.swift
+//  YiLinkerOnlineSeller
+//
+//  Created by Joriel Oller Fronda on 1/29/16.
+//  Copyright (c) 2016 YiLinker. All rights reserved.
+//
+
+import UIKit
+
+class PayoutRequestListModel: NSObject {
+   
+}
