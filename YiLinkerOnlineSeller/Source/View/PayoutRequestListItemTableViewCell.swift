7f5f3dc42a39948eff16d1cdc02faa7b6a471668
Yhel <joriel.fronda@easyshop.ph>
2016-02-01 10:53:14 +0800

    Added new tableview cells for header.

---
+++ YiLinkerOnlineSeller/Source/View/PayoutRequestListItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PayoutRequestListItemTableViewCell.swift
+//  YiLinkerOnlineSeller
+//
+//  Created by Joriel Oller Fronda on 2/1/16.
+//  Copyright (c) 2016 YiLinker. All rights reserved.
+//
+
+import UIKit
+
+class PayoutRequestListItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
