13ca886204260059fc6b5929ec2a981e80503c4f
Yhel <joriel.fronda@easyshop.ph>
2016-02-01 11:18:33 +0800

    Added new header for bank detail in payout request detail.

---
+++ YiLinkerOnlineSeller/Source/View/PayoutRequestListBankDetailHeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PayoutRequestListBankDetailHeaderTableViewCell.swift
+//  YiLinkerOnlineSeller
+//
+//  Created by Joriel Oller Fronda on 2/1/16.
+//  Copyright (c) 2016 YiLinker. All rights reserved.
+//
+
+import UIKit
+
+class PayoutRequestListBankDetailHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
