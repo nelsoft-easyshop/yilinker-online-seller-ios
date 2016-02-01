528d3b2eb007a83ad6561a0ba9c8d5a4276ab482
Rj <rj.constantino@easyshop.ph>
2016-02-01 18:05:33 +0800

    Applied dashboard to payout.

--- YiLinkerOnlineSeller/Source/ViewController/DashboardViewController.swift
+++ YiLinkerOnlineSeller/Source/ViewController/DashboardViewController.swift
@@ -466,12 +466,9 @@ class DashboardViewController: UIViewController, UICollectionViewDataSource, UIC
             transaction.hidesBottomBarWhenPushed = true
             self.navigationController?.pushViewController(transaction, animated: true)
         } else if indexPath.row == 3 {
-//            let productManagement = ProductManagementViewController(nibName: "ProductManagementViewController", bundle: nil)
-//            productManagement.hidesBottomBarWhenPushed = true
-//            self.navigationController?.pushViewController(productManagement, animated: true)
-            let payoutVC = PayoutViewController(nibName: "PayoutViewController", bundle: nil)
-            payoutVC.hidesBottomBarWhenPushed = true
-            self.navigationController?.pushViewController(payoutVC, animated: true)
+            let productManagement = ProductManagementViewController(nibName: "ProductManagementViewController", bundle: nil)
+            productManagement.hidesBottomBarWhenPushed = true
+            self.navigationController?.pushViewController(productManagement, animated: true)
         } else if indexPath.row == 4 {
             var customizedCategory = CustomizedCategoryViewController(nibName: "CustomizedCategoryViewController", bundle: nil)
             self.navigationController?.pushViewController(customizedCategory, animated:true)
@@ -490,13 +487,9 @@ class DashboardViewController: UIViewController, UICollectionViewDataSource, UIC
                 self.tabBarController!.presentViewController(navigationController, animated: true, completion: nil)
             }
         } else if indexPath.row == 6 {
-            /*let payoutViewController: PayoutRequestListViewController = PayoutRequestListViewController(nibName: "PayoutRequestListViewController", bundle: nil)
-            payoutViewController.hidesBottomBarWhenPushed = true
-            self.navigationController?.pushViewController(payoutViewController, animated:true)*/
-            var payoutRequestListDetailViewController = PayoutEarningsViewController(nibName: "PayoutEarningsViewController", bundle: nil)
-            //self.navigationController?.presentViewController(payoutRequestListDetailViewController, animated: true, completion: nil)
-            payoutRequestListDetailViewController.hidesBottomBarWhenPushed = true
-            self.navigationController?.pushViewController(payoutRequestListDetailViewController, animated:true)
+            let payoutVC = PayoutViewController(nibName: "PayoutViewController", bundle: nil)
+            payoutVC.hidesBottomBarWhenPushed = true
+            self.navigationController?.pushViewController(payoutVC, animated: true)
         } else if indexPath.row == 7 {
             var followerController = FollowersViewController(nibName: "FollowersViewController", bundle: nil)
             self.navigationController?.pushViewController(followerController, animated:true)
