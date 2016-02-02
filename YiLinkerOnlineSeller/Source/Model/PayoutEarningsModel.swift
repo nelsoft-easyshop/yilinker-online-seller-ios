1e99af23482bb1cc38331869ef989627c0a5f343
Yhel <joriel.fronda@easyshop.ph>
2016-02-02 14:57:11 +0800

    Refactor models.

--- YiLinkerOnlineSeller/Source/Model/PayoutEarningsModel.swift
+++ YiLinkerOnlineSeller/Source/Model/PayoutEarningsModel.swift
@@ -10,36 +10,52 @@ import UIKit
 
 class PayoutEarningsModel: NSObject {
     
-    var earningType: [String] = []
-    var earningTypeId: [Int] = []
-    var earningAmount: [String] = []
-    var currencyCode: [String] = []
+    var earningType: String = ""
+    var earningTypeId: Int = 0
+    var earningAmount: String = ""
+    var currencyCode: String = ""
     
-    init(earningType: NSArray, earningTypeId: NSArray, earningAmount: NSArray, currencyCode: NSArray) {
-        self.earningType = earningType as! [String]
-        self.earningTypeId = earningTypeId as! [Int]
-        self.earningAmount = earningAmount as! [String]
-        self.currencyCode = currencyCode as! [String]
+    static var payoutEarningsModel: [PayoutEarningsModel] = []
+    
+    init(earningType: String, earningTypeId: Int, earningAmount: String, currencyCode: String) {
+        self.earningType = earningType as String
+        self.earningTypeId = earningTypeId as Int
+        self.earningAmount = earningAmount as String
+        self.currencyCode = currencyCode as String
     }
     
-    class func parseDataWithDictionary(dictionary: AnyObject) -> PayoutEarningsModel {
+    class func parseDataWithDictionary(dictionary: AnyObject) -> [PayoutEarningsModel] {
         
-        var earningType: [String] = []
-        var earningTypeId: [Int] = []
-        var earningAmount: [String] = []
-        var currencyCode: [String] = []
+        var earningType: String = ""
+        var earningTypeId: Int = 0
+        var earningAmount: String = ""
+        var currencyCode: String = ""
         
-        if let request: AnyObject = dictionary["data"] {
-            if let value = request["earningGroups"] as? NSArray {
-                for subValue in value as NSArray {
-                    earningType.append(subValue["name"] as! String)
-                    earningTypeId.append(subValue["earningTypeId"] as! Int)
-                    earningAmount.append(subValue["totalAmount"] as! String)
-                    currencyCode.append(subValue["currencyCode"] as! String)
+        if dictionary.isKindOfClass(NSDictionary) {
+            if let request: AnyObject = dictionary["data"] {
+                if let value = request["earningGroups"] as? NSArray {
+                    for subValue in value as NSArray {
+                        if let tempName = subValue["name"] as? String {
+                            earningType = tempName
+                        }
+                        
+                        if let tempTypeId = subValue["earningTypeId"] as? Int {
+                            earningTypeId = tempTypeId
+                        }
+                        
+                        if let tempTotalAmount = subValue["totalAmount"] as? String {
+                            earningAmount = tempTotalAmount
+                        }
+                        
+                        if let tempCurrencyCode = subValue["currencyCode"] as? String {
+                            currencyCode = tempCurrencyCode
+                        }
+                        self.payoutEarningsModel.append(PayoutEarningsModel(earningType: earningType, earningTypeId: earningTypeId, earningAmount: earningAmount, currencyCode: currencyCode))
+                    }
                 }
             }
         }
         
-        return PayoutEarningsModel(earningType: earningType, earningTypeId: earningTypeId, earningAmount: earningAmount, currencyCode: currencyCode)
+        return self.payoutEarningsModel
     }
 }
