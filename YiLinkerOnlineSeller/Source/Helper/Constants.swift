b9ed10106220dcfe4682476353388aa9dc2ccbb0
Yhel <joriel.fronda@easyshop.ph>
2016-04-19 09:45:47 +0800

    Changed client secret and client id.

--- YiLinkerOnlineSeller/Source/Helper/Constants.swift
+++ YiLinkerOnlineSeller/Source/Helper/Constants.swift
@@ -117,7 +117,7 @@ struct Constants {
         static func getClientId(isSeller: Bool) -> String {
             if isSeller {
                 if APIEnvironment.development {
-                    return "3_4qzm05tv6uwwko4c4c8gs00sco0c40os08owg8sg0wswoo0w8o"
+                    return "1_9t2337riou0wsws84ckw8gkck8os8skw8cokoooc04gc0kssc"
                 } else if APIEnvironment.staging {
                     return "1_167rxzqvid8g8swggwokcoswococscocc8ck44wo0g88owgkcc"
                 } else {
@@ -137,7 +137,7 @@ struct Constants {
         static func getClientSecret(isSeller: Bool) -> String {
             if isSeller {
                 if APIEnvironment.development {
-                    return "1vgsjw5b0u74kssco8cooock0oc8c0sscoksk0sgsc08s8k4gw"
+                    return "1vmep15il4cgw8gc0g8gokokk0wwkko0cg0go0s4c484kwswo4"
                 } else if APIEnvironment.staging {
                     return "317eq8nohry84ooc0o8woo8000c0k844c4cggws84g80scwwog"
                 } else {
