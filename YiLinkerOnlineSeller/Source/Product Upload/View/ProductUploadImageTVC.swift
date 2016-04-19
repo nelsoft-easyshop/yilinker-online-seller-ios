eb79e7ffd9b8ce4266defbf3df53c831e564b585
Yhel <joriel.fronda@easyshop.ph>
2016-04-19 09:51:05 +0800

    Added delegate and datasource methods of collection view.

--- YiLinkerOnlineSeller/Source/Product Upload/View/ProductUploadImageTVC.swift
+++ YiLinkerOnlineSeller/Source/Product Upload/View/ProductUploadImageTVC.swift
@@ -8,17 +8,100 @@
 
 import UIKit
 
-class ProductUploadImageTVC: UITableViewCell {
+// MARK: Constant variable declarations
+struct ProductUploadImageTVCConstant {
+    static let productUploadImageCollectionViewCellNibNameAndIdentifier = "ProductUploadImageTVC"
+}
 
+// MARK: Data source
+// ProductUploadUploadImageTableViewCell Data source methods
+protocol ProductUploadImageTVCDataSource {
+    func productUploadUploadImageTableViewCell(numberOfCollectionViewRows cell: ProductUploadImageTVC) -> Int
+    func productUploadUploadImageTableViewCell(images cell: ProductUploadImageTVC) -> [UIImage]
+}
+
+// MARK: Delegate
+// ProductUploadUploadImageTableViewCell delegate methods
+protocol ProductUploadImageTVCDelegate {
+    func productUploadUploadImageTableViewCell(didSelecteRowAtIndexPath indexPath: NSIndexPath, cell: ProductUploadImageTVC)
+    func productUploadUploadImageTableViewCell(didDeleteAtRowIndexPath indexPath: NSIndexPath, collectionView: UICollectionView)
+}
+
+class ProductUploadImageTVC: UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegate, ProductUploadImageCollectionViewCellDelegate {
+    
+    // Collection view
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // Labels
+    @IBOutlet weak var productPhotosLabel: UILabel!
+    
+    // Initialiaze ProductUploadImageTVCDataSource and ProductUploadImageTVCDelegate
+    var dataSource: ProductUploadImageTVCDataSource?
+    var delegate: ProductUploadImageTVCDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.registerCell()
+        
+        self.productPhotosLabel.text = ProductUploadStrings.productPhotos
+        self.productPhotosLabel.required()
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
+    // MARK: Private methods
+    // Register collection view cell
+    func registerCell() {
+        let nib: UINib = UINib(nibName: ProductUploadImageTVCConstant.productUploadImageCollectionViewCellNibNameAndIdentifier, bundle: nil)
+        self.collectionView.registerNib(nib, forCellWithReuseIdentifier: ProductUploadImageTVCConstant.productUploadImageCollectionViewCellNibNameAndIdentifier)
+    }
+    
+    // MARK: ProductUploadImageTVC delegate method
+    func productUploadImageCollectionViewCell(didTapDeleteButtonAtCell cell: ProductUploadImageCollectionViewCell) {
+        let indexPath: NSIndexPath = self.collectionView.indexPathForCell(cell)!
+        self.delegate!.productUploadUploadImageTableViewCell(didDeleteAtRowIndexPath: indexPath, collectionView: self.collectionView)
+    }
+    
+    // MARK: Collection view delegate methods
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3 //self.dataSource!.productUploadUploadImageTableViewCell(numberOfCollectionViewRows: self)
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: ProductUploadImageCollectionViewCell = self.collectionView.dequeueReusableCellWithReuseIdentifier(ProductUploadImageTVCConstant.productUploadImageCollectionViewCellNibNameAndIdentifier, forIndexPath: indexPath) as! ProductUploadImageCollectionViewCell
+        
+        cell.imageView.image = self.dataSource!.productUploadUploadImageTableViewCell(images: self)[indexPath.row]
+        
+        if indexPath.row == self.dataSource!.productUploadUploadImageTableViewCell(numberOfCollectionViewRows: self) - 1 {
+            cell.closeButton.hidden = true
+            cell.imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        } else {
+            cell.closeButton.hidden = false
+            cell.imageView.contentMode = UIViewContentMode.ScaleAspectFill
+        }
+        
+        cell.delegate = self
+        
+        if SessionManager.isSeller() {
+            cell.userInteractionEnabled = true
+        } else {
+            cell.userInteractionEnabled = false
+        }
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        self.delegate!.productUploadUploadImageTableViewCell(didSelecteRowAtIndexPath: indexPath, cell: self)
+    }
+    
+    // Dealloc
+    deinit {
+        self.collectionView.delegate = nil
+        self.collectionView.dataSource = nil
+    }
 }
