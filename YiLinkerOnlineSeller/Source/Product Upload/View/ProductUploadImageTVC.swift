47e2610da1b667e1ae3c8c5b3589b6fb89e9b5fd
Yhel <joriel.fronda@easyshop.ph>
2016-04-20 10:38:59 +0800

    Initialized label.

--- YiLinkerOnlineSeller/Source/Product Upload/View/ProductUploadImageTVC.swift
+++ YiLinkerOnlineSeller/Source/Product Upload/View/ProductUploadImageTVC.swift
@@ -10,21 +10,23 @@ import UIKit
 
 // MARK: Constant variable declarations
 struct ProductUploadImageTVCConstant {
-    static let productUploadImageCollectionViewCellNibNameAndIdentifier = "ProductUploadImageTVC"
+    static let productUploadImageCollectionViewCellNibNameAndIdentifier = "ProductUploadImageCollectionViewCell"
 }
 
 // MARK: Data source
-// ProductUploadUploadImageTableViewCell Data source methods
-protocol ProductUploadImageTVCDataSource {
+// ProductUploadUploadImageTVC Data source methods
+protocol ProductUploadUploadImageTVCDataSource {
     func productUploadUploadImageTableViewCell(numberOfCollectionViewRows cell: ProductUploadImageTVC) -> Int
     func productUploadUploadImageTableViewCell(images cell: ProductUploadImageTVC) -> [UIImage]
 }
 
 // MARK: Delegate
-// ProductUploadUploadImageTableViewCell delegate methods
-protocol ProductUploadImageTVCDelegate {
+// ProductUploadUploadImageTVC delegate methods
+protocol ProductUploadUploadImageTVCDelegate {
     func productUploadUploadImageTableViewCell(didSelecteRowAtIndexPath indexPath: NSIndexPath, cell: ProductUploadImageTVC)
     func productUploadUploadImageTableViewCell(didDeleteAtRowIndexPath indexPath: NSIndexPath, collectionView: UICollectionView)
+    
+    func productUploadUploadImageTableViewCell(didTapStarAtRowIndexPath indexPath: NSIndexPath, cell: ProductUploadImageCollectionViewCell, collectionView: UICollectionView)
 }
 
 class ProductUploadImageTVC: UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegate, ProductUploadImageCollectionViewCellDelegate {
@@ -34,14 +36,15 @@ class ProductUploadImageTVC: UITableViewCell, UICollectionViewDataSource, UIColl
     
     // Labels
     @IBOutlet weak var productPhotosLabel: UILabel!
+    @IBOutlet weak var noteLabel: UILabel!
     
     // Initialiaze ProductUploadImageTVCDataSource and ProductUploadImageTVCDelegate
-    var dataSource: ProductUploadImageTVCDataSource?
-    var delegate: ProductUploadImageTVCDelegate?
+    var dataSource: ProductUploadUploadImageTVCDataSource?
+    var delegate: ProductUploadUploadImageTVCDelegate?
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.registerCell()
+        self.registerNib()
         
         self.productPhotosLabel.text = ProductUploadStrings.productPhotos
         self.productPhotosLabel.required()
@@ -53,22 +56,38 @@ class ProductUploadImageTVC: UITableViewCell, UICollectionViewDataSource, UIColl
         // Configure the view for the selected state
     }
     
-    // MARK: Private methods
-    // Register collection view cell
-    func registerCell() {
+    // MARK: -
+    // MARK: - Local methods
+    // MARK: - Register collection view cell
+    
+    func registerNib() {
         let nib: UINib = UINib(nibName: ProductUploadImageTVCConstant.productUploadImageCollectionViewCellNibNameAndIdentifier, bundle: nil)
         self.collectionView.registerNib(nib, forCellWithReuseIdentifier: ProductUploadImageTVCConstant.productUploadImageCollectionViewCellNibNameAndIdentifier)
     }
     
-    // MARK: ProductUploadImageTVC delegate method
+    // MARK: -
+    // MARK: - ProductUploadImageTVC delegate method
+    // MARK: - Tap the Delete button
+    
     func productUploadImageCollectionViewCell(didTapDeleteButtonAtCell cell: ProductUploadImageCollectionViewCell) {
         let indexPath: NSIndexPath = self.collectionView.indexPathForCell(cell)!
         self.delegate!.productUploadUploadImageTableViewCell(didDeleteAtRowIndexPath: indexPath, collectionView: self.collectionView)
     }
     
-    // MARK: Collection view delegate methods
+    // MARK: - Tap the star button
+    
+    func productUploadImageCollectionViewCell(didTapStarButtonAtCell cell: ProductUploadImageCollectionViewCell) {
+        cell.starButton.setBackgroundImage(UIImage(named: "active2"), forState: UIControlState.Normal)
+        let indexPath: NSIndexPath = self.collectionView.indexPathForCell(cell)!
+        self.delegate!.productUploadUploadImageTableViewCell(didTapStarAtRowIndexPath: indexPath, cell: cell, collectionView: self.collectionView)
+        self.collectionView.reloadData()
+    }
+    
+    // MARK: -
+    // MARK: - Collection view delegate methods
+    
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3 //self.dataSource!.productUploadUploadImageTableViewCell(numberOfCollectionViewRows: self)
+        return self.dataSource!.productUploadUploadImageTableViewCell(numberOfCollectionViewRows: self)
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
