61d13fd9f033d92b59369140a857c276441d03ba
Rj <rj.constantino@easyshop.ph>
2016-04-21 17:03:58 +0800

    Implemented commission layout

--- YiLinkerOnlineSeller/Source/Product Management/Controller/CommisionViewController.swift
+++ YiLinkerOnlineSeller/Source/Product Management/Controller/CommisionViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class CommisionViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var commissionTextField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.title = "Commision"
+        self.edgesForExtendedLayout = UIRectEdge.None
+        self.saveButton.layer.cornerRadius = 2.0
     }
     
+    // MARK: - Actions
+    
+    @IBAction func saveCommissionAction(sender: UIButton) {
+        
+        self.navigationController?.popViewControllerAnimated(true)
+    }
 }
