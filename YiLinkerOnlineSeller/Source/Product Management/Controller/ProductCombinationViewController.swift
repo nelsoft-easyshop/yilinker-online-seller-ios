904d504f03eb2d62361814c203a3f2cd3770ce43
Rj <rj.constantino@easyshop.ph>
2016-04-21 18:46:41 +0800

    Implemented product combination layout.

--- YiLinkerOnlineSeller/Source/Product Management/Controller/ProductCombinationViewController.swift
+++ YiLinkerOnlineSeller/Source/Product Management/Controller/ProductCombinationViewController.swift
@@ -10,27 +10,115 @@ import UIKit
 
 class ProductCombinationViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let cellTitles = ["Finish", "Storage", "SKU"]
+    let cellValues = ["Silver", "16GB", "G86712835-12"]
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.title = "Product Combinations"
+        
+        setupTableView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Functions
+    
+    func setupTableView() {
+        
+        self.tableView.backgroundColor = Constants.Colors.backgroundGray
+        
+        let headerView: UIView = UIView(frame: CGRectMake(0, 0, self.tableView.frame.size.width, 10.0))
+//        headerView.backgroundColor = UIColor.lightGrayColor()
+        self.tableView.tableHeaderView = headerView
+        
+        let footerView: UIView = UIView(frame: CGRectMake(0, 0, self.tableView.frame.size.width, 80.0))
+//        footerView.backgroundColor = UIColor.lightGrayColor()
+        let saveButton: UIButton = UIButton(frame: CGRectMake(15, 0, self.tableView.frame.size.width - 30, 50.0))
+        saveButton.backgroundColor = Constants.Colors.pmCheckGreenColor
+        saveButton.titleLabel!.font = UIFont(name: "Helvetica-Neue", size: 12.0)
+        saveButton.layer.cornerRadius = 2.0
+        saveButton.setTitle("SAVE PRODUCT COMBINATIONS", forState: .Normal)
+        saveButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        saveButton.addTarget(self, action: "saveAction", forControlEvents: .TouchUpInside)
+        footerView.addSubview(saveButton)
+        self.tableView.tableFooterView = footerView
+        
+        // registering cells
+        
+        self.tableView.registerNib(UINib(nibName: "ProductCombinationTableViewCell", bundle: nil), forCellReuseIdentifier: "combinationCell")
+        self.tableView.registerNib(UINib(nibName: "ProductCombination2TableViewCell", bundle: nil), forCellReuseIdentifier: "combinationCell2")
+        self.tableView.registerNib(UINib(nibName: "ProductCombination3TableViewCell", bundle: nil), forCellReuseIdentifier: "combinationCell3")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - Actions
+    
+    func saveAction() {
+        
     }
-    */
+    
+}
 
+extension ProductCombinationViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    // MARK: - Table View Data Source
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        if indexPath.row == 3 {
+            let cell: ProductCombination2TableViewCell = tableView.dequeueReusableCellWithIdentifier("combinationCell2") as! ProductCombination2TableViewCell
+            return cell
+        } else if indexPath.row == 4 {
+            let cell: ProductCombination3TableViewCell = tableView.dequeueReusableCellWithIdentifier("combinationCell3") as! ProductCombination3TableViewCell
+            return cell
+        }
+        
+        let cell: ProductCombinationTableViewCell = tableView.dequeueReusableCellWithIdentifier("combinationCell") as! ProductCombinationTableViewCell
+        cell.titleLabel.text = cellTitles[indexPath.row]
+        cell.valueLabel.text = cellValues[indexPath.row]
+        return cell
+    }
+    
+    // MARK: - Table View Delegate
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let headerView: UIView = UIView(frame: CGRectMake(0, 0, self.tableView.frame.size.width, 40.0))
+        headerView.backgroundColor = UIColor.whiteColor()
+        
+        let textLabel: UILabel = UILabel(frame: CGRectMake(15, 0, headerView.frame.size.width - 15, headerView.frame.size.height))
+        textLabel.text = "Combination \(section + 1)"
+        headerView.addSubview(textLabel)
+        
+        return headerView
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40.0
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        if indexPath.row == 3 {
+            return 89.0
+        } else if indexPath.row == 4 {
+            return 70.0
+        }
+        
+        return 44.0
+    }
+    
 }
---
+++ YiLinkerOnlineSeller/Source/Product Management/View/ProductCombinationTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ProductCombinationTableViewCell.swift
+//  YiLinkerOnlineSeller
+//
+//  Created by Rj Constantino on 4/21/16.
+//  Copyright (c) 2016 YiLinker. All rights reserved.
+//
+
+import UIKit
+
+class ProductCombinationTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
