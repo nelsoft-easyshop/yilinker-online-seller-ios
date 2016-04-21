df128aea15c2d1addc725cb9feaa927498fd64fe
Yhel <joriel.fronda@easyshop.ph>
2016-04-21 18:10:07 +0800

    Modified.

--- YiLinkerOnlineSeller/Source/Product Upload/View/ProductUploadCombinationFooterTVC.swift
+++ YiLinkerOnlineSeller/Source/Product Upload/View/ProductUploadCombinationFooterTVC.swift
@@ -11,10 +11,17 @@ import UIKit
 protocol ProductUploadCombinationFooterTVCDelegate {
     func productUploadSkuDimensionsAndWeightTableViewCell(textFieldDidChange textField: UITextField, text: String, cell:
         ProductUploadCombinationFooterTVC)
+    func productUploadCombinationFooterTVC(didClickUploadImage cell: ProductUploadCombinationFooterTVC)
+    func productUploadCombinationFooterTVC(didDeleteUploadImage cell: ProductUploadCombinationFooterTVC, indexPath: NSIndexPath)
+    func productUploadCombinationFooterTVC(didClickDoneButton cell: ProductUploadCombinationFooterTVC, sku: String, length: String, width: String, height: String, weight: String, uploadImages: [UIImage])
+    
 }
 
-class ProductUploadCombinationFooterTVC: UITableViewCell, UITextFieldDelegate {
+class ProductUploadCombinationFooterTVC: UITableViewCell, UITextFieldDelegate, UICollectionViewDataSource, UICollectionViewDelegate, ProductUploadImageCollectionViewCellDelegate {
 
+    // CollectionView
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     // Labels
     @IBOutlet weak var skuLabel: UILabel!
     @IBOutlet weak var lengthLabel: UILabel!
@@ -33,13 +40,20 @@ class ProductUploadCombinationFooterTVC: UITableViewCell, UITextFieldDelegate {
     // Switch
     @IBOutlet weak var availableSwitch: UISwitch!
     
-    // Variables 
+    // Variables
+    var viewController: UIViewController?
+    
+    // Global variables
+    var images: [UIImage] = []
+    
     var delegate: ProductUploadCombinationFooterTVCDelegate?
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         self.addTextFieldDelegate()
+        self.registerCell()
         
         // Set labels texts
         self.lengthLabel.text = ProductUploadStrings.length
@@ -60,6 +74,57 @@ class ProductUploadCombinationFooterTVC: UITableViewCell, UITextFieldDelegate {
         // Configure the view for the selected state
     }
     
+    @IBAction func save(sender: AnyObject) {
+        //if self.images.last == UIImage(named: "addPhoto") {
+        self.images.removeLast()
+        //}
+        
+        if self.lengthTextField.text == "" {
+            UIAlertController.displayErrorMessageWithTarget(viewController!, errorMessage: "Lenght is required.", title: ProductUploadStrings.incompleteProductDetails)
+        } else if self.widthTextField.text == "" {
+            UIAlertController.displayErrorMessageWithTarget(viewController!, errorMessage: "Width is required.", title: ProductUploadStrings.incompleteProductDetails)
+        } else if self.skuTextField.text == "" {
+            UIAlertController.displayErrorMessageWithTarget(viewController!, errorMessage: ProductUploadStrings.skuRequried, title: ProductUploadStrings.incompleteProductDetails)
+        } else {
+            if self.viewController != nil {
+                viewController!.view.endEditing(true)
+            }
+            
+            let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(0.3 * Double(NSEC_PER_SEC)))
+            dispatch_after(delayTime, dispatch_get_main_queue()) {
+                self.delegate?.productUploadCombinationFooterTVC(didClickDoneButton: self, sku: self.skuTextField.text, length: self.lengthTextField.text, width: self.widthTextField.text, height: self.heightTextField.text, weight: self.weightTextField.text, uploadImages: self.images)
+            }
+        }
+    }
+    
+    // MARK: Collection view delegate and data source methods
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: ProductUploadImageCollectionViewCell = self.collectionView.dequeueReusableCellWithReuseIdentifier(ProductUploadUploadImageTableViewCellConstant.productUploadImageCollectionViewCellNibNameAndIdentifier, forIndexPath: indexPath) as! ProductUploadImageCollectionViewCell
+        cell.delegate = self
+        cell.imageView.image = self.images[indexPath.row]
+        cell.starButton.hidden = true 
+
+        if indexPath.row == self.images.count - 1 {
+            cell.closeButton.hidden = true
+            cell.imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        } else {
+            cell.closeButton.hidden = false
+            cell.imageView.contentMode = UIViewContentMode.ScaleAspectFill
+        }
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.row == self.images.count - 1 {
+            self.delegate?.productUploadCombinationFooterTVC(didClickUploadImage: self)
+        }
+    }
+    
     // MARK: -
     // MARK: - Local methods
     // MARK: - Add textfield delegate actions
@@ -76,6 +141,22 @@ class ProductUploadCombinationFooterTVC: UITableViewCell, UITextFieldDelegate {
     }
     
     // MARK: -
+    // MARK: - Register Cell
+    
+    func registerCell() {
+        let nib: UINib = UINib(nibName: ProductUploadUploadImageTableViewCellConstant.productUploadImageCollectionViewCellNibNameAndIdentifier, bundle: nil)
+        self.collectionView.registerNib(nib, forCellWithReuseIdentifier: ProductUploadUploadImageTableViewCellConstant.productUploadImageCollectionViewCellNibNameAndIdentifier)
+    }
+    
+    // MARK: -
+    // MARK: - Uploaded Images
+    
+    func uploadedImages() -> [UIImage] {
+        self.images.removeLast()
+        return self.images
+    }
+    
+    // MARK: -
     // MARK: - Textfield data source methods
     
     func textFieldDidChange(sender: UITextField) {
@@ -85,5 +166,23 @@ class ProductUploadCombinationFooterTVC: UITableViewCell, UITextFieldDelegate {
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         self.endEditing(true)
         return true
+    }
+    
+    // MARK: ProductUploadImageCollectionViewCell Delegate method
+    func productUploadImageCollectionViewCell(didTapDeleteButtonAtCell cell: ProductUploadImageCollectionViewCell) {
+        let indexPath: NSIndexPath = self.collectionView.indexPathForCell(cell)!
+        self.images.removeAtIndex(indexPath.item)
+        self.collectionView.deleteItemsAtIndexPaths([indexPath])
+        self.delegate!.productUploadCombinationFooterTVC(didDeleteUploadImage: self, indexPath: indexPath)
+    }
+    
+    func productUploadImageCollectionViewCell(didTapStarButtonAtCell cell: ProductUploadImageCollectionViewCell) {
+        cell.starButton.setBackgroundImage(UIImage(named:"active2"), forState: UIControlState.Normal)
+    }
+    
+    // Dealloc
+    deinit {
+        self.collectionView.delegate = nil
+        self.collectionView.dataSource = nil
     }
 }
