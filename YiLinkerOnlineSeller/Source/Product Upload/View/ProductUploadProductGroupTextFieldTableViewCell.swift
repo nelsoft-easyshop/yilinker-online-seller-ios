8e3cf27268c6b3f3c978d6cf295f1487a63af819
Yhel <joriel.fronda@easyshop.ph>
2016-06-03 15:53:38 +0800

    Added new tableview cell for product group.

---
+++ YiLinkerOnlineSeller/Source/Product Upload/View/ProductUploadProductGroupTextFieldTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductUploadProductGroupTextFieldTableViewCell.swift
+//  YiLinkerOnlineSeller
+//
+//  Created by Joriel Oller Fronda on 6/3/16.
+//  Copyright (c) 2016 YiLinker. All rights reserved.
+//
+
+import UIKit
+
+class ProductUploadProductGroupTextFieldTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
