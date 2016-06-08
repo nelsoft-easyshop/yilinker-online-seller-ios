954690a1e285091ca980a15e9f8a596218893ca8
Yhel <joriel.fronda@easyshop.ph>
2016-06-08 18:01:55 +0800

    Set font, changed background color.

--- YiLinkerOnlineSeller/Source/Warehouse/ViewControllers/WarehouseFilterViewController.swift
+++ YiLinkerOnlineSeller/Source/Warehouse/ViewControllers/WarehouseFilterViewController.swift
@@ -83,9 +83,9 @@ class WarehouseFilterViewController: UIViewController {
         
         //let view = UIView(frame: CGRect.zeroRect)
         let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 40))
-        view.backgroundColor = UIColor.lightGrayColor()
+        view.backgroundColor = Constants.Colors.backgroundGray
         let label = UILabel(frame: CGRect(x: 10, y: 5, width: tableView.frame.size.width, height: 20))
-        
+        label.font = UIFont(name: "Panton", size: 12.0)
         if self.warehouseFilter != nil {
             label.text = self.warehouseFilter!.filter[section]
         }
@@ -119,7 +119,7 @@ class WarehouseFilterViewController: UIViewController {
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cellIdentifier: String = "FilterCell"
         let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! UITableViewCell
-        
+        cell.textLabel.font = UIFont(name: "Panton", size: 12.0)
         if self.warehouseFilter != nil {
             cell.textLabel!.text = self.warehouseFilter!.filterModel[indexPath.section].name[indexPath.row]
         }
