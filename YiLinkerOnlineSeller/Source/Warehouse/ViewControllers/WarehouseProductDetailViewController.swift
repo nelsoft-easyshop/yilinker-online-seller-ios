68180d65898e82e2d888959d1852512c7767a403
Yhel <joriel.fronda@easyshop.ph>
2016-06-09 15:54:32 +0800

    Implemented api in inventory.

--- YiLinkerOnlineSeller/Source/Warehouse/ViewControllers/WarehouseProductDetailViewController.swift
+++ YiLinkerOnlineSeller/Source/Warehouse/ViewControllers/WarehouseProductDetailViewController.swift
@@ -20,6 +20,7 @@ class WarehouseProductDetailViewController: UIViewController {
     @IBOutlet weak var actualInventoryTextField: UITextField!
     
     var warehouseProduct: WarehouseInventoryProduct!
+    var warehouseId: String = ""
     
     override func viewDidLoad() {
         self.title = "Warehouse Product Detail"
@@ -60,6 +61,95 @@ class WarehouseProductDetailViewController: UIViewController {
     
     func save() {
         println("save product edit, API CALL")
+        if self.actualInventoryTextField.text == "" {
+            self.showAlert("Please add actual inventory.", message: "")
+        } else {
+            self.fireUpdateInventory()
+        }
+    }
+    
+    // MARK: -
+    // MARK: - Alert view
+    
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        
+        let OKAction = UIAlertAction(title: Constants.Localized.ok, style: .Default) { (action) in
+            //self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+        alertController.addAction(OKAction)
+        
+        self.presentViewController(alertController, animated: true) {
+        }
+    }
+    
+    // MARK: -
+    // MARK: - POST METHOD: Refresh token
+    /*
+    *
+    * (Parameters) - client_id, client_secret, grant_type, refresh_token
+    *
+    * Function to refresh token to get another access token
+    *
+    */
+    
+    func fireRefreshToken() {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        
+        WebServiceManager.fireRefreshTokenWithUrl(APIAtlas.refreshTokenUrl, actionHandler: {
+            (successful, responseObject, requestErrorType) -> Void in
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+            if successful {
+                SessionManager.parseTokensFromResponseObject(responseObject as! NSDictionary)
+                self.fireUpdateInventory()
+            } else {
+                //Show UIAlert and force the user to logout
+                UIAlertController.displayAlertRedirectionToLogin(self, actionHandler: { (sucess) -> Void in
+                })
+            }
+        })
+    }
+    
+    func fireUpdateInventory() {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        
+        var parameters: NSMutableDictionary  = ["warehouseId" : self.warehouseId, "productUnit" : self.warehouseProduct.productUnitId, "quantity" : self.actualInventoryTextField.text]
+        
+        WebServiceManager.fireAddWarehouseAddressRequestWithUrl(APIAtlas.addWarehouse + SessionManager.accessToken(), parameters: parameters, actionHandler: { (successful, responseObject, requestErrorType) -> Void in
+            if successful {
+                if let success = responseObject["isSuccessful"] as? Bool {
+                    if success {
+                        Toast.displayToastWithMessage(responseObject["message"] as! String, duration: 1.5, view: self.view)
+                    }
+                }
+                
+                UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+            } else {
+                UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                
+                if requestErrorType == .ResponseError {
+                    //Error in api requirements
+                    let errorModel: ErrorModel = ErrorModel.parseErrorWithResponce(responseObject as! NSDictionary)
+                    self.showAlert(Constants.Localized.error, message: errorModel.message)
+                } else if requestErrorType == .AccessTokenExpired {
+                    self.fireRefreshToken()
+                } else if requestErrorType == .PageNotFound {
+                    //Page not found
+                    Toast.displayToastWithMessage(Constants.Localized.pageNotFound, duration: 1.5, view: self.view)
+                } else if requestErrorType == .NoInternetConnection {
+                    //No internet connection
+                    Toast.displayToastWithMessage(Constants.Localized.noInternetErrorMessage, duration: 1.5, view: self.view)
+                } else if requestErrorType == .RequestTimeOut {
+                    //Request timeout
+                    Toast.displayToastWithMessage(Constants.Localized.noInternetErrorMessage, duration: 1.5, view: self.view)
+                } else if requestErrorType == .UnRecognizeError {
+                    //Unhandled error
+                    UIAlertController.displayErrorMessageWithTarget(self, errorMessage: Constants.Localized.someThingWentWrong, title: Constants.Localized.error)
+                }
+            }
+        })
+
     }
 
 }
