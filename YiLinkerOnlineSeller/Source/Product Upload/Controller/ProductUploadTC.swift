c234afd5acc36156010a6bed951feafaeff0aea1
Yhel <yhel@Admins-MacBook-Pro.local>
2016-06-10 17:43:21 +0800

    Fixed bugs issue 1060.

--- YiLinkerOnlineSeller/Source/Product Upload/Controller/ProductUploadTC.swift
+++ YiLinkerOnlineSeller/Source/Product Upload/Controller/ProductUploadTC.swift
@@ -878,6 +878,22 @@ class ProductUploadTC: UITableViewController, ProductUploadUploadImageTVCDataSou
             }
             self.productModel.productMainImagesModel[i] = productMainImagesModel!
         }
+        self.cellImage?.collectionView.reloadData()
+    }
+    
+    func updateModelReupload() {
+        for i in 0..<self.productModel.productMainImagesModel.count {
+            var productMainImagesModel: ProductMainImagesModel?
+            if self.productModel.productMainImagesModel[i].imageStatus == true && self.productModel.productMainImagesModel[i].imageFailed == false {
+                productMainImagesModel = ProductMainImagesModel(image: self.productModel.productMainImagesModel[i].image, imageName: self.productModel.productMainImagesModel[i].imageName, imageStatus: true, imageFailed: false)
+            } else if self.productModel.productMainImagesModel[i].imageStatus == false && self.productModel.productMainImagesModel[i].imageFailed == true {
+                productMainImagesModel = ProductMainImagesModel(image: self.productModel.productMainImagesModel[i].image, imageName: self.productModel.productMainImagesModel[i].imageName, imageStatus: false, imageFailed: false)
+            } else if self.productModel.productMainImagesModel[i].imageStatus == false && self.productModel.productMainImagesModel[i].imageFailed == true {
+                productMainImagesModel = ProductMainImagesModel(image: self.productModel.productMainImagesModel[i].image, imageName: self.productModel.productMainImagesModel[i].imageName, imageStatus: false, imageFailed: false)
+            }
+            self.productModel.productMainImagesModel[i] = productMainImagesModel!
+        }
+        self.cellImage?.collectionView.reloadData()
     }
     
     // MARK: -
@@ -945,6 +961,7 @@ class ProductUploadTC: UITableViewController, ProductUploadUploadImageTVCDataSou
     }
     
     func productUploadUploadImageTableViewCell(didTapReuploadAtRowIndexPath indexPath: NSIndexPath, cell: ProductUploadImageCollectionViewCell, collectionView: UICollectionView) {
+        self.updateModelReupload()
         self.fireUploadProductMainImages(self.productModel.productMainImagesModel[indexPath.row].image)
     }
     
