a389f9e986879aa4b9d6138d0283b387113b1d3a
Rj <rj.constantino@yilinker.ph>
2016-06-15 14:26:34 +0800

    Resolved crash when response is not array.

--- YiLinkerOnlineSeller/Source/Product Management/Controller/ProductManagementViewController.swift
+++ YiLinkerOnlineSeller/Source/Product Management/Controller/ProductManagementViewController.swift
@@ -461,6 +461,10 @@ class ProductManagementViewController: UIViewController, ProductManagementModelV
                     //Error in api requirements
 
                     let errorModel: ErrorModel = ErrorModel.parseErrorWithResponce(responseObject as! NSDictionary)
+//                    let responseData: NSDictionary = responseObject["data"] as! NSDictionary
+
+//                    println(responseData)
+                    
 //                    let data: NSArray = responseObject["data"] as! NSArray
 //                    if data == NSArray() {
 //                        self.emptyLabel.hidden = false
